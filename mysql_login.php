5bdd900bbf7cf610bf155d474e965d3f0ce0d42b
FlightlessParrot <113798246+FlightlessParrot@users.noreply.github.com>
2022-09-30 13:05:16 +0200

    Ignorowanie htaccess

---
+++ mysql_login.php
@@ -0,0 +1,12 @@
+<?php //mysql_login.php
+$server='localhost';
+$dbname='users';
+$admin='root';
+$admin_pass='Valpoqw123#';
+$chrf='utf8mb4';
+$attr="mysql:host=$server;dbname=$dbname;charset=$chrf";
+$opts=
+[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+PDO::ATTR_EMULATE_PREPARES=>FALSE,];
+?>
